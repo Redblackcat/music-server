0269a646817b8f332276dde99e0a100d30a349c0
chenzixin <1106915891@qq.com>
2023-03-07 18:41:36 +0800

    添加接口测试类

--- src/test/java/com/guoran/TestController.java
+++ src/test/java/com/guoran/TestController.java
@@ -9,14 +9,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.swing.*;
 
 /**
  * 接口测试类
